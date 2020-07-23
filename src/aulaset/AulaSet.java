845ae6633f224a617377089201bdd8321f77201e
Victor Wanderley <victor.wanderley1709@gmail.com>
2020-07-23 16:51:34 -0300

    Adicionando for each pra impressão das notas

--- src/aulaset/AulaSet.java
+++ src/aulaset/AulaSet.java
@@ -17,13 +17,17 @@ public class AulaSet {
     public static void main(String[] args) {
         // TODO code application logic here
         Set<Double>  notasAlunos = new HashSet<>();
-        
-        notasAlunos.add(5.8);        
+        notasAlunos.add(10.0);
+        notasAlunos.add(6.3);        
         notasAlunos.add(7.9);
         notasAlunos.add(9.0);
         notasAlunos.add(8.5);
         
         System.out.println(notasAlunos);
+        
+        for(double nota: notasAlunos){
+            System.out.println(nota);
+        }
     }
     
 }
